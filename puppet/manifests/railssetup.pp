56e045bc0373ec803ee5219ac1bb8120ba6dac6b
Salimane Adjao Moustapha <me@salimane.com>
2013-06-06 20:48:06 +0800

    separate railssetup into rubysetup + rails

--- puppet/manifests/railssetup.pp
+++ puppet/manifests/railssetup.pp
@@ -1,62 +1,17 @@
 # == Class: railssetup
 #
-class railssetup ($username = 'vagrant') {
+class railssetup ($username = 'vagrant',  $rubyversion = '1.9.3-p392') {
 
     $home_dir    = "/home/${username}"
-    $rubyversion = '1.9.3-p392'
-    #package {['libreadline5-dev']: ensure => 'installed'}
 
-    rbenv::install { $username:
-        group => $username,
-        user  => $username,
-        home  => $home_dir,
+    import 'rubysetup.pp'
+
+    class { 'rubysetup':
+        username    => $username,
+        rubyversion =>  $rubyversion
     }
 
-    rbenv::plugin {
-        'rbenv-gem-rehash':
-            group  => $username,
-            user   => $username,
-            source => 'git://github.com/sstephenson/rbenv-gem-rehash.git';
-
-        'rbenv-vars':
-            group  => $username,
-            user   => $username,
-            source => 'git://github.com/sstephenson/rbenv-vars.git';
-
-        'rbenv-each':
-            group  => $username,
-            user   => $username,
-            source => 'git://github.com/chriseppstein/rbenv-each.git';
-
-        'rbenv-update':
-            group  => $username,
-            user   => $username,
-            source => 'git://github.com/rkh/rbenv-update.git';
-
-        'rbenv-whatis':
-            group  => $username,
-            user   => $username,
-            source => 'git://github.com/rkh/rbenv-whatis.git';
-
-        'rbenv-use':
-            group  => $username,
-            user   => $username,
-            source => 'git://github.com/rkh/rbenv-use.git';
-
-        'rbenv-default-gems':
-            group  => $username,
-            user   => $username,
-            source => 'git://github.com/sstephenson/rbenv-default-gems.git';
-    }
-
-    rbenv::compile { $rubyversion:
-        group  => $username,
-        user   => $username,
-        home   => $home_dir,
-        global => true
-    }
-
-    rbenv::gem { ['specific_install', 'rails', 'bundle', 'unicorn', 'capistrano']:
+    rbenv::gem { ['rails', 'bundle', 'unicorn', 'capistrano']:
         ensure  => latest,
         user    => $username,
         ruby    => $rubyversion,
---
+++ puppet/manifests/rubysetup.pp
@@ -0,0 +1,65 @@
+# == Class: railssetup
+#
+class rubysetup ($username = 'vagrant',  $rubyversion = '1.9.3-p392') {
+
+    $home_dir    = "/home/${username}"
+
+    #package {['libreadline5-dev']: ensure => 'installed'}
+
+    rbenv::install { $username:
+        group => $username,
+        user  => $username,
+        home  => $home_dir,
+    }
+
+    rbenv::plugin {
+        'rbenv-gem-rehash':
+            group  => $username,
+            user   => $username,
+            source => 'git://github.com/sstephenson/rbenv-gem-rehash.git';
+
+        'rbenv-vars':
+            group  => $username,
+            user   => $username,
+            source => 'git://github.com/sstephenson/rbenv-vars.git';
+
+        'rbenv-each':
+            group  => $username,
+            user   => $username,
+            source => 'git://github.com/chriseppstein/rbenv-each.git';
+
+        'rbenv-update':
+            group  => $username,
+            user   => $username,
+            source => 'git://github.com/rkh/rbenv-update.git';
+
+        'rbenv-whatis':
+            group  => $username,
+            user   => $username,
+            source => 'git://github.com/rkh/rbenv-whatis.git';
+
+        'rbenv-use':
+            group  => $username,
+            user   => $username,
+            source => 'git://github.com/rkh/rbenv-use.git';
+
+        'rbenv-default-gems':
+            group  => $username,
+            user   => $username,
+            source => 'git://github.com/sstephenson/rbenv-default-gems.git';
+    }
+
+    rbenv::compile { $rubyversion:
+        group  => $username,
+        user   => $username,
+        home   => $home_dir,
+        global => true
+    }
+
+    rbenv::gem { ['specific_install']:
+        ensure  => latest,
+        user    => $username,
+        ruby    => $rubyversion,
+        require => Rbenv::Compile[$rubyversion],
+    }
+}
